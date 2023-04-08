19d821868eaeb9820b75f69a04e49e8340cd4e61
1m1 <email@1m1.io>
2023-04-08 15:02:43 -0600

    example fully working (A separated)

--- example/run.sh
+++ example/run.sh
@@ -1,14 +1,26 @@
 export FROM_ACC=5B3SUGACYLICWU3DHXYCS45NDNEFZCZM4MCKCKQA3DLGKZEOFQR74HLGEU
 export LP_ACC=UDFWT5DW3X5RZQYXKQEMZ6MRWAEYHWYP7YUAPZKPW6WJK3JH3OZPL7PO2Y
 export LP_APP=148607000
+
 # A
-goal app create --creator $FROM_ACC --approval-prog A.teal --clear-prog A.teal --global-byteslices 0 --global-ints 0 --local-byteslices 0 --local-ints 0 --on-completion NoOp --app-account $LP_ACC --foreign-app $LP_APP
-export A=178596639
+tealish compile A.tl && cp build/A.teal .
+goal app create --creator $FROM_ACC --approval-prog A.teal --clear-prog A.teal --global-byteslices 0 --global-ints 0 --local-byteslices 0 --local-ints 0
+export A=178895540
 goal app update --app-id $A --from $FROM_ACC --approval-prog A.teal --clear-prog A.teal
 goal app call --app-id $A --from $FROM_ACC --foreign-app $LP_APP --app-account $LP_ACC
 
 # B
-goal app create --creator $FROM_ACC --approval-prog B.teal --clear-prog B.teal --global-byteslices 0 --global-ints 0 --local-byteslices 0 --local-ints 0 --on-completion NoOp --foreign-app $A --foreign-app $LP_APP --app-account $LP_ACC --fee 100000
-export B=178596899
-goal app update --app-id $B --from $FROM_ACC --approval-prog B.teal --clear-prog B.teal --foreign-app $A --foreign-app $A --foreign-app $LP_APP --app-account $LP_ACC --fee 100000
-goal app call --app-id $B --from $FROM_ACC --foreign-app $A --foreign-app $LP_APP --app-account $LP_ACC --fee 100000
+tealish compile B.tl && cp build/B.teal .
+goal app create --creator $FROM_ACC --approval-prog B.teal --clear-prog B.teal --global-byteslices 0 --global-ints 1 --local-byteslices 0 --local-ints 0
+export B=178907864
+goal app update --app-id $B --from $FROM_ACC --approval-prog B.teal --clear-prog B.teal
+
+# call
+goal app call --app-id $A --from $FROM_ACC --foreign-app $LP_APP --app-account $LP_ACC --out A.txn
+goal app call --app-id $B --from $FROM_ACC --out B.txn
+cat A.txn B.txn > combined.txn
+goal clerk group --infile combined.txn --outfile call.txn
+goal clerk sign --infile call.txn --outfile call.stxn
+goal clerk rawsend --filename call.stxn
+goal clerk dryrun -t call.stxn --dryrun-dump -o dryrun.json
+tealdbg debug B.teal -d dryrun.json --group-index 1
